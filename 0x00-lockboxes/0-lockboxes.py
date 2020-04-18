0896ff3d923e67f9610669d3e0241488168884b0
sranciato <sranciato1@gmail.com>
2020-04-18 16:50:33 -0400

    [UPDATE] add description for method

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(list):
+    """ returns true if all boxes can be unlocked """
     length = len(list)
     count = 1
     for box_index in range(length):
         for key_index in range(len(list[box_index])):
             try:
                 nextBox = list[list[box_index][key_index]]
-            except:
+            except Exception as e:
                 continue
             if len(nextBox) == 0 and count == length - 1:
                 return True
             try:
                 del list[box_index][key_index]
                 count += 1
-            except:
+            except Exception as e:
                 pass
             box_index = nextBox
-            
-        
     return False
-        
