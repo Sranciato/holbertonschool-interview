89ffc975d1ecb3b365b39347a40b64cea231a42a
sranciato <sranciato1@gmail.com>
2020-05-05 05:29:34 -0400

    [BETTY] fix betty

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,16 +7,29 @@ def canUnlockAll(list):
     count = 1
     for box_index in range(length):
         for key_index in range(len(list[box_index])):
+            print(key_index)
+            print(list)
             try:
                 nextBox = list[list[box_index][key_index]]
             except Exception as e:
                 continue
             if len(nextBox) == 0 and count == length - 1:
                 return True
+            elif len(nextBox) == 0 and count != length - 1:
+                print("HEYY {}".format(len(list[box_index])))
+                # if (len(list[box_index]) == 1):
+                #     return False
+                continue
             try:
                 del list[box_index][key_index]
                 count += 1
             except Exception as e:
                 pass
             box_index = nextBox
+            print("after {}".format(list))
+
+    print("FINAl list {}".format(list))
+    print("final count {}".format(count))
+    if (count == length):
+        return True
     return False
--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -2,17 +2,17 @@
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
+# boxes = [[1], [-5, 2], [3], [4], []]
+# print(canUnlockAll(boxes))
 
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
+# boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+# print(canUnlockAll(boxes))
 
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+# boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+# print(canUnlockAll(boxes))
 
 boxes = [[3], [2], [], [1]]
 print(canUnlockAll(boxes))
 
-boxes = [[1,3,5], [0, 4, 1], [], [], [], [1]]
-print(canUnlockAll(boxes))
+# boxes = [[1,3,5], [0, 4, 1], [], [], [], [1]]
+# print(canUnlockAll(boxes))
