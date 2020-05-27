786dac6a99a4627d012de3bcd475701e4d8cb8a2
sranciato <sranciato1@gmail.com>
2020-05-27 14:55:56 -0400

    [ADD] palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * is_palindrome - checks if list is palindrome.
+ * @head: beginning of list.
+ * Return: 1 if true, 0 if false.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *start = *head, *length = *head;
+	listint_t *end = *head, *restart = *head;
+	int len = 0, i = 0, j;
+
+	if (!length)
+		return (1);
+	while (length)
+	{
+		len++;
+		length = length->next;
+	}
+	while (i < len / 2)
+	{
+		j = i;
+		while (j < len - 1)
+		{
+			end = end->next;
+			j++;
+		}
+		if (start->n != end->n)
+			return (0);
+		end = restart;
+		start = start->next;
+		i++;
+	}
+	return (1);
+}
