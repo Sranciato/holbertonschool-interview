5717ea06c43e5dcd43c6c8da997a8ed816f50861
sranciato <sranciato1@gmail.com>
2020-05-27 14:57:05 -0400

    [ADD]

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -5,7 +5,7 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
-n = 11
+n = -2   
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
 n = 12
