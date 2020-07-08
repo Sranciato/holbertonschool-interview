4eab8708c64d2079c9ac5b1f7f677ecd719cbbbe
sranciato <sranciato1@gmail.com>
2020-07-08 07:02:31 -0400

    [INIT] menger spooonge

---
+++ 0x0B-menger/menger.c
@@ -0,0 +1,12 @@
+#include "menger.h"
+/**
+ * menger - Function that draws a 2d menger sponge
+ * @level: the level of the menger sponge
+ */
+void menger(int level)
+{
+    for (int i = 0; i <= level; i++)
+    {
+        printf("\n");
+    }
+}
