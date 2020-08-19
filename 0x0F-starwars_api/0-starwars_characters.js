f0eee4025b23a6e4ab3267c319bea4c2f2b234d6
sranciato <sranciato1@gmail.com>
2020-08-19 16:08:29 -0400

    [ADD] semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,23 +4,23 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
 function prom(character){
-    return new Promise(function(resolve, reject){
-        request(character, function(error, response, body) {
-            if (error) {
-                reject(error);
-            }
-            const name = JSON.parse(body).name;
-            resolve(name);
-        })
-    })
+  return new Promise(function(resolve, reject){
+    request(character, function(error, response, body) {
+      if (error) {
+        reject(error);
+      }
+        const name = JSON.parse(body).name;
+        resolve(name);
+    });
+  });
 }
 
 request(url + process.argv[2], async function (error, response, body) {
-    if (error) {
-        console.log(error);
-    }
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-        console.log(await prom(character));
-    }
+  if (error) {
+    console.log(error);
+  }
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    console.log(await prom(character));
+  }
 });
