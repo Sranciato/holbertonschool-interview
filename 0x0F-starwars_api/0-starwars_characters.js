757f407ad3806e5747615b057d73c44007363539
sranciato <sranciato1@gmail.com>
2020-08-19 16:05:56 -0400

    [INIT] first push

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+function prom(character){
+    return new Promise(function(resolve, reject){
+        request(character, function(error, response, body) {
+            if (error) {
+                reject(error);
+            }
+            const name = JSON.parse(body).name;
+            resolve(name);
+        })
+    })
+}
+
+request(url + process.argv[2], async function (error, response, body) {
+    if (error) {
+        console.log(error);
+    }
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+        console.log(await prom(character));
+    }
+});
