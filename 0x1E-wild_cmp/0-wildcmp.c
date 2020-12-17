33bc7af07e12d8dcf74ea7c0172feb6a2048e107
sranciato <sranciato1@gmail.com>
2020-12-16 21:11:56 -0500

    [INIT] First push

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: returns 1 if the strings can be considered identical
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 != *s2)
+	{
+		if (*s2 == '*')
+		{
+			if (*(s1 + 1) == *(s2 + 1))
+			{
+				return (wildcmp(s1 + 1, s2 + 1));
+			}
+			if (*(s1 + 1) != *(s2 + 1) && *(s1 + 1) != '\0')
+			{
+				if (*(s2 + 1) != '*')
+				{
+					return (wildcmp(s1 + 1, s2));
+				}
+				if (*(s2 + 1) == '*')
+				{
+					return (wildcmp(s1, s2 + 1));
+				}
+			}
+			return (1);
+		}
+		return (0);
+	}
+	if (*s1 == *s2)
+	{
+		if (*s1 == '\0' || *s2 == '\0')
+			return (1);
+
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (11);
+}
